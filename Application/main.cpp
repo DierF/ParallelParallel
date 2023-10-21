1021f60c6f0d94ce59029526f262f5afae78ec8e
Marshall Feng <fengdier2@126.com>
2023-10-20 20:57:29 -0400

    Prepare to start over with Opengl.

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,22 +1,8 @@
 #include <iostream>
 
-#include "Application.h"
-
 int
 main()
 {
-	PParllel::Application app;
-
-	try
-	{
-		app.run();
-	}
-	catch (std::exception const& e)
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-
 	std::cout << "Hi" << std::endl;
 
 	return EXIT_SUCCESS;
